0ddb8cda95b4f4f6f2a481f3eea00dd6ec53002c
Lillian Wang <110583549+wang-lillian@users.noreply.github.com>
2025-06-10 18:14:00 -0400

    Added logic for fetching articles

---
+++ services/fetch_articles.py
@@ -0,0 +1,63 @@
+from dotenv import load_dotenv
+import os
+from sentence_transformers import SentenceTransformer, util
+import json
+import urllib.request
+from urllib.parse import urlencode
+
+load_dotenv()
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def fetch_articles(user_interest: str) -> list:
+    interest = [user_interest]
+    categories = [
+        "general",
+        "world",
+        "nation",
+        "business",
+        "technology",
+        "entertainment",
+        "sports",
+        "science",
+        "health",
+    ]
+
+    categories_embeddings = model.encode(categories, normalize_embeddings=True)
+    interest_embedding = model.encode(interest, normalize_embeddings=True)
+
+    similarities = util.cos_sim(categories_embeddings, interest_embedding).tolist()
+    top_similarities_indices = sorted(
+        range(len(categories)), key=lambda i: similarities[i], reverse=True
+    )[:3]
+    interest_categories = [categories[i] for i in top_similarities_indices]
+
+    articles_to_chunk = []
+    for category in interest_categories:
+        articles = fetch_top_headlines(category)
+        articles_to_chunk.extend(articles)
+
+    return articles_to_chunk
+
+
+def fetch_top_headlines(category: str) -> list:
+    gnews_api_key = os.getenv("GNEWS_API_KEY")
+    params = {
+        "category": category,
+        "lang": "en",
+        "country": "us",
+        "max": "5",
+        "from": "2025-05-10T00:00:00Z",
+        "apikey": gnews_api_key,
+    }
+    gnews_url = f"https://gnews.io/api/v4/top-headlines?{urlencode(params)}"
+
+    try:
+        with urllib.request.urlopen(gnews_url) as response:
+            data = json.loads(response.read().decode("utf-8"))
+            articles = data.get("articles", [])
+            return articles
+    except Exception as e:
+        print(f"Error fetching {category} articles: {str(e)}")
+        return []
+
+
