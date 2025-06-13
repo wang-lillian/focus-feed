bace3adcd6f3572c1b6258d2e69363c53bae5a33
Lillian Wang <110583549+wang-lillian@users.noreply.github.com>
2025-06-13 00:44:58 -0400

    Added scheduled indexing jobs with APScheduler and refresh endpoint

--- main.py
+++ main.py
@@ -2,11 +2,36 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from contextlib import asynccontextmanager
+from apscheduler.schedulers.background import BackgroundScheduler
+from threading import Lock
+import uvicorn
 from services.search import search
 from services.process_articles import index_documents
-import uvicorn
 
-app = FastAPI()
+
+interest_lock = Lock()
+latest_user_interest = None
+scheduler = BackgroundScheduler()
+
+def scheduled_indexing():
+    with interest_lock:
+        if latest_user_interest:
+            print(f"[Scheduler] Indexing more documents for: {latest_user_interest}")
+            index_documents(latest_user_interest)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler.add_job(scheduled_indexing, "interval", hours=1)
+    scheduler.start()
+    print("Scheduler started at startup")
+    yield
+    scheduler.shutdown()
+    print("Scheduler shut down")
+
+
+app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
@@ -17,8 +42,15 @@ def render_home(request: Request):
 
 
 @app.post("/read", response_class=HTMLResponse)
-def render_read(request: Request, user_interest: str = Form(...)):
-    index_documents(user_interest)
+def render_read(request: Request, user_interest: str = Form(...), action: str = Form(...)):
+    global latest_user_interest
+    if action == "submit":
+        with interest_lock:
+            latest_user_interest = user_interest.strip().lower()
+        index_documents(user_interest)
+    elif action == "refresh":
+        print("refreshed!")
+
     results = search(user_interest)
     return templates.TemplateResponse(
         request=request,
@@ -27,5 +59,6 @@ def render_read(request: Request, user_interest: str = Form(...)):
     )
 
 
+
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run("main:app", reload=True)
